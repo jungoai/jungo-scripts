0c7087ba4a2a6fc27571402e550933b8ccb4eb75
Mohsen <thelight.mn@gmail.com>
2025-07-07 18:43:12 +0330

    feat: add nix package

--- flake.nix
+++ flake.nix
@@ -8,6 +8,8 @@
     let
       pkgs = import nixpkgs { inherit system; };
       hPkgs = pkgs.haskell.packages."ghc984";
+
+      jungochainRunPkg = hPkgs.callCabal2nix "jungochain-run" ./. { };
     in
     {
       devShells.default = pkgs.mkShell {
@@ -18,8 +20,16 @@
           hPkgs.cabal-install
 
           pkgs.zlib
-          pkgs.pkg-config # need to find zlib
+          pkgs.pkg-config # need for finding zlib
         ];
+      };
+
+      packages.default = jungochainRunPkg;
+
+      # Define a `nix run` app (if your project produces an executable)
+      apps.default = {
+        type = "app";
+        program = "${jungochainRunPkg}/bin/jungochain-run"; # Replace with your executable name
       };
     });
 }
